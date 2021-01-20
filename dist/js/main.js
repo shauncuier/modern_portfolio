f2e72561c3959f6f9216ab253663c2c18993f1a9
Shaun <shauncuier@gmail.com>
2021-01-21 03:02:11 +0600

    index markup and main sass

---
+++ dist/js/main.js
@@ -0,0 +1,20 @@
+// Select DOM Items
+const menuBtn = document.querySelector('.btn-menu');
+const menu = document.querySelector('.menu');
+const menuNev = document.querySelector('.menu-nav');
+const menuBranging = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+// Set Initial State of Menu
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        
+    }else{
+        
+    }
+}
