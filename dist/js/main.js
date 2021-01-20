e38ac6a7882e049a8577edb179e65c3efb0e92a8
Shaun <shauncuier@gmail.com>
2021-01-21 03:48:29 +0600

    Menu Button javaScript, Line Rotated

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,5 @@
 // Select DOM Items
-const menuBtn = document.querySelector('.btn-menu');
+const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNev = document.querySelector('.menu-nav');
 const menuBranging = document.querySelector('.menu-branding');
@@ -13,8 +13,22 @@ menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
     if (!showMenu) {
-        
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNev.classList.add('show');
+        menuBranging.classList.add('show');
+        navItems.forEach(item=> item.classList.add('show'));
+
+        // Set Menu State
+        showMenu = true;
     }else{
-        
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNev.classList.remove('show');
+        menuBranging.classList.remove('show');
+        navItems.forEach(item=> item.classList.remove('show'));
+
+        // Set Menu State
+        showMenu = false;
     }
 }
